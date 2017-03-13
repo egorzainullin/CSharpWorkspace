d415835063ba7ac7c5faf73ae35ffee5c1fa654c
egorzainullin <egor.zainullin@icloud.com>
2017-03-13 19:01:35 +0300

    -дописаны тесты к списку

--- HashTable/HashTable/HashTableTests/ListTest.cs
+++ HashTable/HashTable/HashTableTests/ListTest.cs
@@ -53,25 +53,45 @@ namespace HashTableTests
         [TestMethod]
         public void ClearTest()
         {
-
+            var list = new List();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Clear();
+            Assert.AreEqual(null, list.Head);
         }
 
         [TestMethod]
         public void DeleteFromHeadTest()
         {
-
+            var list = new List();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.DeleteFromHead();
+            Assert.AreEqual(2, list.Pop());
         }
 
         [TestMethod]
         public void isContainingTest1()
         {
-
+            var list = new List();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.AreEqual(true, list.IsContaining(2));
+            Assert.AreEqual(false, list.IsContaining(5));
         }
 
         [TestMethod]
         public void RemoveTest()
         {
-
+            var list = new List();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Remove(1);
+            Assert.AreEqual(false, list.IsContaining(1));
         }
     }
 }
