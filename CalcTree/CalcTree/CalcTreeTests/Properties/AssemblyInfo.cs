1b657d3cc66fd820f01b6e4b9b9b6a602a71ed5b
egorzainullin <egor.zainullin@icloud.com>
2017-03-29 19:57:12 +0300

    -добавлены пару новых тестов

---
+++ CalcTree/CalcTree/CalcTreeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcTreeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcTreeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aed32377-7672-44ca-bc62-05907df01054")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcTree/CalcTree/CalcTreeTests/TreeTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculationTree;
+
+namespace CalcTreeTests
+{
+    [TestClass]
+    public class TreeTests
+    {
+        [TestMethod]
+        public void CalcExpressionTest1()
+        {
+            var calcTree = new CalcTree("(* (+ 1 1) 2)");
+            Assert.AreEqual(4, calcTree.Calculate());
+        }
+
+        [TestMethod]
+        public void CalcExpressionTest2()
+        {
+            var calcTree = new CalcTree("(* (- 2 1) (+ 1 1))");
+            Assert.AreEqual(2, calcTree.Calculate());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void InvalidFormatTest()
+        {
+            var calcTree = new CalcTree("(*(+ 1 1) 2)");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZeroTest()
+        {
+            var calcTree = new CalcTree("(/ 1 0)");
+            calcTree.Calculate();
+        }
+
+        [TestMethod]
+        public void PrintTest()
+        {
+            var calcTree = new CalcTree("(* (+ 1 1) 2)");
+            Assert.AreEqual("(* (+ 1 1) 2)", calcTree.Print());
+        }                    
+    }
+}
+
