53c52833cc8bb053b8fe23226224b80cd09c3ade
egorzainullin <egor.zainullin@icloud.com>
2017-03-30 23:15:13 +0300

    -первый релиз

---
+++ FuncMapFilerFold/FuncMapFilerFoldTests/HandfulOfFunctionsTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FuncMapFilerFold.Tests
+{
+    [TestClass()]
+    public class HandfulOfFunctionsTests
+    {
+        private List<int> list;
+
+        private HandfulOfFunctions functions;
+
+        [TestInitialize()]
+        public void InitTest()
+        {
+            list = new List<int> { 1, 2, 3 };
+            functions = new HandfulOfFunctions();
+        }
+
+        [TestMethod()]
+        public void MapTest()
+        {
+            int[] array = functions.Map(list, x => x * 2).ToArray();
+            Assert.AreEqual(2, array[0]);
+            Assert.AreEqual(4, array[1]);
+            Assert.AreEqual(6, array[2]);
+        }
+
+        [TestMethod()]
+        public void FilterTest()
+        {
+            int[] array = functions.Filter(list, x => x % 2 == 0).ToArray();
+            Assert.AreEqual(2, array[0]);
+        }
+
+        [TestMethod()]
+        public void FoldTest()
+        {
+            int res = functions.Fold(list, 1, (acc, elem) => acc* elem);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
