ae94446c5051c2b751e19e893f35293f0725c5a3
egorzainullin <egor.zainullin@icloud.com>
2017-04-02 14:50:16 +0300

    -Сделана игрушка -не совсем понятно как к ней сделать тесты

---
+++ EventLoop/EventLoop/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Events
+{
+    internal class Game
+    {
+        internal static void OnDown(object sender, EventArgs e)
+        {
+            ++Console.CursorTop;
+        }
+
+        internal static void OnUp(object sender, EventArgs e)
+        {
+            if (Console.CursorTop > 0)
+            {
+                --Console.CursorTop;
+            }
+        }
+
+        internal static void OnLeft(object sender, EventArgs e)
+        {
+            if (Console.CursorLeft > 0)
+            {
+                --Console.CursorLeft;
+            }
+        }
+
+        internal static void OnRight(object sender, EventArgs e)
+        {
+            ++Console.CursorLeft;
+        }
+    }
+}
---
+++ EventLoop/EventLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            eventLoop.LeftHandler += Game.OnLeft;
+            eventLoop.RightHandler += Game.OnRight;
+            eventLoop.UpHandler += Game.OnUp;
+            eventLoop.DownHandler += Game.OnDown;
+            eventLoop.Run();
+        }
+    }
+}
