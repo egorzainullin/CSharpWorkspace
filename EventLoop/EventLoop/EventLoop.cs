07c0dcbf39f39bb6e812b918b56f33f0382775c3
egorzainullin <egor.zainullin@icloud.com>
2017-04-02 17:21:37 +0300

    -добавлены документация

--- EventLoop/EventLoop/EventLoop.cs
+++ EventLoop/EventLoop/EventLoop.cs
@@ -6,12 +6,31 @@ using System.Threading.Tasks;
 
 namespace Events
 {
+    /// <summary>
+    /// Инициализирует класс <see cref="EventLoop"/>, необходимый для создания бесконечной цикла событий
+    /// </summary>
     public class EventLoop
     {
+        /// <summary>
+        /// Событие, возникающее при нажатии стрелочки налево
+        /// </summary>
         public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+        /// <summary>
+        /// Событие, возникающее при нажатии стрелочки вправо
+        /// </summary>
         public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+        /// <summary>
+        /// Событие, возникающее при нажатии стрелочки вверх
+        /// </summary>
         public event EventHandler<EventArgs> UpHandler = (sender, args) => { };
+        /// <summary>
+        /// Событие, возникающее при нажатии стрелочки вниз
+        /// </summary>
         public event EventHandler<EventArgs> DownHandler = (sender, args) => { };
+
+        /// <summary>
+        /// Метод, запускающий игру
+        /// </summary>
         public void Run()
         {
             bool exit = false;
--- EventLoop/EventLoop/Game.cs
+++ EventLoop/EventLoop/Game.cs
@@ -2,13 +2,26 @@
 
 namespace Events
 {
+    /// <summary>
+    /// Класс, реализующий бизнес логику игры
+    /// </summary>
     internal class Game
     {
+        /// <summary>
+        /// Идти вниз при на нажатии стрелочки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         internal static void OnDown(object sender, EventArgs e)
         {
             ++Console.CursorTop;
         }
 
+        /// <summary>
+        /// Идти вверх при на нажатии стрелочки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         internal static void OnUp(object sender, EventArgs e)
         {
             if (Console.CursorTop > 0)
@@ -17,6 +30,11 @@ namespace Events
             }
         }
 
+        /// <summary>
+        /// Идти влево при на нажатии стрелочки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         internal static void OnLeft(object sender, EventArgs e)
         {
             if (Console.CursorLeft > 0)
@@ -25,6 +43,11 @@ namespace Events
             }
         }
 
+        /// <summary>
+        /// Идти вправо при на нажатии стрелочки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         internal static void OnRight(object sender, EventArgs e)
         {
             ++Console.CursorLeft;
