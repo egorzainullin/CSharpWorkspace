b74cfabdf94113fba8e6f265ac0c785c077edd30
egorzainullin <egor.zainullin@icloud.com>
2017-04-07 11:07:23 +0300

    -первая версия калькулятора

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Calculator
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label0 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.getAnswerButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.backspaceButton = new System.Windows.Forms.Button();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.operatorLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.label0, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.plusButton, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.minusButton, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.getAnswerButton, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.divideButton, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.multiplyButton, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.clearButton, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.backspaceButton, 0, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 154);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 328);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label0
+            // 
+            this.label0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label0.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label0.Location = new System.Drawing.Point(78, 196);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(67, 64);
+            this.label0.TabIndex = 26;
+            this.label0.Text = "0";
+            this.label0.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label0.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label9
+            // 
+            this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(152, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 64);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label9.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label8
+            // 
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(78, 131);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 64);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label8.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label7
+            // 
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(4, 131);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 64);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label7.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label6
+            // 
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(152, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 64);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(78, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 64);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(4, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 64);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(152, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 64);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(78, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 64);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 64);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.OnNumberLabelClick);
+            // 
+            // plusButton
+            // 
+            this.plusButton.Location = new System.Drawing.Point(226, 134);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(70, 58);
+            this.plusButton.TabIndex = 3;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.OnOperatorButtonClick);
+            // 
+            // minusButton
+            // 
+            this.minusButton.Location = new System.Drawing.Point(226, 199);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(70, 58);
+            this.minusButton.TabIndex = 4;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.OnOperatorButtonClick);
+            // 
+            // getAnswerButton
+            // 
+            this.getAnswerButton.Location = new System.Drawing.Point(226, 264);
+            this.getAnswerButton.Name = "getAnswerButton";
+            this.getAnswerButton.Size = new System.Drawing.Size(70, 58);
+            this.getAnswerButton.TabIndex = 5;
+            this.getAnswerButton.TabStop = false;
+            this.getAnswerButton.Text = "=";
+            this.getAnswerButton.UseVisualStyleBackColor = true;
+            this.getAnswerButton.Click += new System.EventHandler(this.OnGetAnswerClick);
+            // 
+            // divideButton
+            // 
+            this.divideButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.divideButton.Location = new System.Drawing.Point(226, 69);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(70, 58);
+            this.divideButton.TabIndex = 2;
+            this.divideButton.Text = ":";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.OnOperatorButtonClick);
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.multiplyButton.Location = new System.Drawing.Point(226, 4);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(70, 58);
+            this.multiplyButton.TabIndex = 1;
+            this.multiplyButton.Text = "*";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.OnOperatorButtonClick);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(4, 199);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(67, 58);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.OnClearButtonClick);
+            // 
+            // backspaceButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.backspaceButton, 3);
+            this.backspaceButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backspaceButton.Location = new System.Drawing.Point(4, 264);
+            this.backspaceButton.Name = "backspaceButton";
+            this.backspaceButton.Size = new System.Drawing.Size(215, 60);
+            this.backspaceButton.TabIndex = 7;
+            this.backspaceButton.Text = "backspace";
+            this.backspaceButton.UseVisualStyleBackColor = true;
+            this.backspaceButton.Click += new System.EventHandler(this.OnBackspaceButtonClick);
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.answerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answerLabel.Location = new System.Drawing.Point(53, 82);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(245, 73);
+            this.answerLabel.TabIndex = 0;
+            this.answerLabel.Text = "0";
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // operatorLabel
+            // 
+            this.operatorLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.operatorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.operatorLabel.Location = new System.Drawing.Point(2, 82);
+            this.operatorLabel.Name = "operatorLabel";
+            this.operatorLabel.Size = new System.Drawing.Size(45, 73);
+            this.operatorLabel.TabIndex = 3;
+            this.operatorLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 494);
+            this.Controls.Add(this.operatorLabel);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Calc";
+            this.Text = "Calculator";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnCalcKeyUp);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button getAnswerButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Label operatorLabel;
+        private System.Windows.Forms.Button backspaceButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,202 @@
+﻿//todo expception divide by zero
+//to do tooomany symbol handler
+
+using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calc : Form
+    {
+        private int currentValue = 0;
+        private int currentAnswer = 0;
+        private string previousOperator = "";
+
+        public Calc()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+
+        private void OnNumberLabelClick(object sender, EventArgs e)
+        {
+            if (previousOperator == "=")
+            {
+                previousOperator = "";
+                currentValue = 0;
+                UpdateScreen();
+            }
+            var label = sender as Label;
+            int value = Int32.Parse(label.Text);
+            UpdateValue(value);
+            UpdateScreen();
+        }
+
+        private void UpdateScreen()
+        {
+            operatorLabel.Text = previousOperator;
+            answerLabel.Text = currentValue.ToString();
+        }
+
+        private void OnOperatorButtonClick(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            string currentOperator = button.Text;
+            if (previousOperator == "")
+            {
+                currentAnswer = currentValue;
+            }
+            CalculateCurrentAnswer();
+            currentValue = 0;
+            previousOperator = currentOperator;
+            UpdateScreen();
+        }
+
+        private void CalculateCurrentAnswer()
+        {
+            switch (previousOperator)
+            {
+                case "+":
+                    currentAnswer += currentValue;
+                    break;
+                case "-":
+                    currentAnswer -= currentValue;
+                    break;
+                case "*":
+                    currentAnswer *= currentValue;
+                    break;
+                case ":":
+                    currentAnswer /= currentValue;
+                    break;
+                case "=":
+                    currentValue = currentAnswer;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void OnGetAnswerClick(object sender, EventArgs e)
+        {
+            operatorLabel.Text = "=";
+            if (previousOperator == "=")
+            {
+                answerLabel.Text = currentAnswer.ToString();
+                return;
+            }
+            if (previousOperator == "")
+            {
+                currentAnswer = currentValue;
+            }
+            else
+            {
+                CalculateCurrentAnswer();
+            }
+            previousOperator = "=";
+            currentValue = 0;
+            answerLabel.Text = currentAnswer.ToString();
+        }
+
+        private void OnClearButtonClick(object sender, EventArgs e)
+        {
+            previousOperator = "";
+            currentValue = 0;
+            currentAnswer = 0;
+            UpdateScreen();
+        }
+
+        private void OnBackspaceButtonClick(object sender, EventArgs e)
+        {
+            if (currentValue > 0)
+            {
+                currentValue = currentValue / 10;
+            }
+            UpdateScreen();
+        }
+
+        private void UpdateValue(int value)
+        {
+            currentValue = currentValue * 10 + value;
+            UpdateScreen();
+        }
+
+        private void OnCalcKeyUp(object sender, KeyEventArgs e)
+        {
+            var key = e.KeyCode;
+            if (e.Shift)
+            {
+                switch (key)
+                {
+                    case Keys.D8:
+                        OnOperatorButtonClick(multiplyButton, EventArgs.Empty);
+                        break;
+                    case Keys.Oemplus:
+                        OnOperatorButtonClick(plusButton, EventArgs.Empty);
+                        break;
+                    case Keys.D6:
+                        OnOperatorButtonClick(divideButton, EventArgs.Empty);
+                        break;
+                    default:
+                        break;
+                }
+                return;
+            }
+            switch (key)
+            {
+                case Keys.D1:
+                    UpdateValue(1);
+                    break;
+                case Keys.D2:
+                    UpdateValue(2);
+                    break;
+                case Keys.D3:
+                    UpdateValue(3);
+                    break;
+                case Keys.D4:
+                    UpdateValue(4);
+                    break;
+                case Keys.D5:
+                    UpdateValue(5);
+                    break;
+                case Keys.D6:
+                    UpdateValue(6);
+                    break;
+                case Keys.D7:
+                    UpdateValue(7);
+                    break;
+                case Keys.D8:
+                    UpdateValue(8);
+                    break;
+                case Keys.D9:
+                    UpdateValue(9);
+                    break;
+                case Keys.D0:
+                    UpdateValue(0);
+                    break;
+                case Keys.Back:
+                    OnBackspaceButtonClick(backspaceButton, EventArgs.Empty);
+                    break;
+                case Keys.C:
+                    OnClearButtonClick(clearButton, EventArgs.Empty);
+                    break;
+                case Keys.Oemplus:
+                    OnGetAnswerClick(getAnswerButton, EventArgs.Empty);
+                    break;
+                case Keys.Enter:
+                    OnGetAnswerClick(getAnswerButton, EventArgs.Empty);
+                    break;
+                case Keys.OemMinus:
+                    OnOperatorButtonClick(minusButton, EventArgs.Empty);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void OnAntiEnterKeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
+
