b2f0f42fc82f47b4afb5fa3fd02b730881943f30
egorzainullin <egor.zainullin@icloud.com>
2017-04-15 18:40:26 +0300

    -добавлена иерархия классов

--- CalcTree/CalcTree/CalcTree/CalcTree.cs
+++ CalcTree/CalcTree/CalcTree/CalcTree.cs
@@ -10,7 +10,7 @@ namespace CalculationTree
         /// <summary>
         /// Корень дерева
         /// </summary>
-        private TreeElement root;
+        private AbstractNode root;
 
         /// <summary>
         /// Считает значение дерева
@@ -21,20 +21,25 @@ namespace CalculationTree
         /// <summary>
         /// Печатает дерево
         /// </summary>
-        public string Print() => root.PrintTree();
+        public string Print()
+        {
+            string outString = root.PrintTree();
+            Console.WriteLine(outString);
+            return outString;
+        }
         
         /// <summary>
         /// Генерирует дерево по заданному выражению
         /// </summary>
         /// <param name="expression"></param>
         /// <returns></returns>
-        private TreeElement GenerateTree(string expression)
+        private AbstractNode GenerateTree(string expression)
         {
             if (expression[0] != '(')
             {
-                return new TreeElement(expression);
+                return new NumberNode(expression);
             }
-            string op = expression[1].ToString();
+            char op = expression[1];
             string leftOperand = "";
             string rightOperand = "";
             if (expression[2] != ' ')
@@ -80,7 +85,7 @@ namespace CalculationTree
             }
             var left = GenerateTree(leftOperand);
             var right = GenerateTree(rightOperand);
-            return new TreeElement(op, left, right);
+            return new OperatorNode(op, left, right);
         }
 
         /// <summary>
@@ -90,124 +95,6 @@ namespace CalculationTree
         public CalcTree(string expression)
         {
             root = GenerateTree(expression);
-        }
-
-        /// <summary>
-        /// Элемент дерева: оператор или число
-        /// </summary>
-        private class TreeElement
-        {
-            /// <summary>
-            /// Свойство: значение в вершине
-            /// </summary>
-            public string Value => value;
-
-            /// <summary>
-            /// Поле значение в вершине
-            /// </summary>
-
-            private string value;
-
-            /// <summary>
-            /// Проверяет, является ли вершина оператором
-            /// </summary>
-            /// <returns></returns>
-            public bool IsOperator() => value == "+" || value == "-" || value == "*" || value == "/";
-
-            /// <summary>
-            /// Левый ребенок оператора
-            /// </summary>
-            private TreeElement leftChild;
-
-            /// <summary>
-            /// Правый ребенок оператора
-            /// </summary>
-            private TreeElement rightChild;
-
-            /// <summary>
-            /// Сохраняет дерево в строку, начиная с root
-            /// </summary>
-            /// <param name="root"></param>
-            private string PrintTree(TreeElement root)
-            {
-                if (root == null)
-                {
-                    return "";
-                }
-                string outString = "";
-                if (root.IsOperator())
-                {
-                    outString = "(" + root.Value + " " + PrintTree(root.leftChild) + " " + PrintTree(root.rightChild) + ")";
-                }
-                else
-                {
-                    outString = root.Value;
-                }
-                return outString;
-            }
-
-            /// <summary>
-            /// Печатает дерево, начиная с этой вершины
-            /// </summary>
-            /// <param name="root"></param>
-            public string PrintTree()
-            {
-                string outString = PrintTree(this);
-                Console.WriteLine(outString);
-                return outString;
-            }
-
-            /// <summary>
-            /// Посчитать значение, начиная с этой вершины
-            /// </summary>
-            /// <returns>Посчитанное значение</returns>
-            public int Calculate()
-            {
-                switch (value)
-                {
-                    case "+":
-                        return leftChild.Calculate() + rightChild.Calculate();
-                        break;
-                    case "*":
-                        return leftChild.Calculate() * rightChild.Calculate();
-                        break;
-                    case "-":
-                        return leftChild.Calculate() - rightChild.Calculate();
-                        break;
-                    case "/":
-                        return leftChild.Calculate() / rightChild.Calculate();
-                        break;
-                    default:
-                        return Int32.Parse(value);
-                        break;
-                }
-            }
-
-            /// <summary>
-            /// Инициализирует новый экземпляр класса <see cref="TreeElement"/>
-            /// </summary>
-            /// <param name="value">Оператор</param>
-            /// <param name="leftChild">Ссылка левый операнд</param>
-            /// <param name="rightChild">Ссылка на правый операнд</param>
-            public TreeElement(string value, TreeElement leftChild, TreeElement rightChild)
-            {
-                this.value = value;
-                this.leftChild = leftChild;
-                this.rightChild = rightChild;
-            }
-
-            /// <summary>
-            /// Инициализирует новый экземпляр класса <see cref="TreeElement"/>
-            /// </summary>
-            /// <param name="value">Число</param>
-            public TreeElement(string value)
-                : this(value, null, null)
-            { }
-
-            /// <summary>
-            /// Печатает оператор / операнд на консоль
-            /// </summary>
-            public void Print() => Console.Write(value);
         }
     }
 }
