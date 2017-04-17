4fe376cbe19ac4ed6293a03af78a370ef9a3790f
egorzainullin <egor.zainullin@icloud.com>
2017-04-17 21:33:53 +0300

    -первая версия программы

---
+++ Stack_List/StackList/StackListTests/ListTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackList.Tests
+{
+    [TestClass()]
+    public class ListTests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteFromHeadTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PopTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PeekTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsContainingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsEmptyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Stack_List/StackList/StackListTests/StackTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackList.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+        [TestMethod()]
+        public void PushTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PopTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PeekTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsEmptyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
